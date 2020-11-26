1fab3d89fb59954528248aa4f7f7aa5f359bbf84
Nikola Stojšin <nikola.stojsin@hotmail.com>
2020-11-26 03:14:46 +0100

    Examples update

---
+++ examples/dvsku.Wpf.Examples/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using dvsku.Wpf.Themes;
+
+namespace dvsku.Wpf.Examples.ViewModels {
+    public class MainWindowViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Theme> _themes;
+        public ObservableCollection<Theme> Themes {
+            get => _themes;
+            set {
+                _themes = value;
+                RaisePropertyChanged(nameof(Themes));
+            }
+        }
+        private Theme _selectedTheme;
+        public Theme SelectedTheme {
+            get => _selectedTheme;
+            set {
+                _selectedTheme = value;
+                ThemeManager.Instance.SetTheme(_selectedTheme);
+                RaisePropertyChanged(nameof(SelectedTheme));
+            }
+        }
+
+        public MainWindowViewModel() {
+            ThemeManager.Instance.SetTheme("System");
+            Themes = new ObservableCollection<Theme>(ThemeManager.Instance.Themes);
+            SelectedTheme = ThemeManager.Instance.CurrentTheme;
+        }
+
+        public void RaisePropertyChanged(string property) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
